284630d60bf72e6ce7d4be793fff7115c801fe6e
lakshay taneja <lakshaytaneja26@gmail.com>
2019-04-11 14:53:06 -0500

    basic structure in place

---
+++ agent.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+batch_size = 10 # every how many episodes to do a param update?
+learning_rate = 1e-4
+gamma = 0.99 # discount factor for reward
+decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
+resume = False # resume from previous checkpoint?
+itr_no = 0
+model_save_path = 'models/{1}.pth'.format(itr_no)
+
+
+class PolicyNetwork(nn.Module):
+    def __init__(self):
+        super(PolicyNetwork, self).__init__()
+        self.in_features = 20
+        self.out_features = 4
+        self.linear1 = nn.Linear(self.in_features, 100)
+        self.linear2 = nn.Linear(100, 40)
+        self.linear3 = nn.Linear(40, self.out_features)
+    
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.softmax(self.linear3(x))
+        return x
+    
+    def discount_rewards(self, r):
+        """ take 1D float array of rewards and compute discounted reward """
+        discounted_r = np.zeros_like(r)
+        running_add = 0
+        for t in reversed(range(0, r.size)):
+            running_add = running_add * gamma + r[t]
+            discounted_r[t] = running_add
+        return discounted_r
+
+model = None    
+
+if resume:
+    model = PolicyNetwork()
+    model.load_state_dict(torch.load(model_save_path))
+else:
+    model = PolicyNetwork()
+
+
+
+
+
+
+
+
+
+
+
--- game.py
+++ game.py
@@ -1,12 +1,67 @@
 import gym, obstacle_env, random
+from agent import model,batch_size, model_save_path
+import numpy as np
+import torch
+render = False
 
 if __name__ == '__main__':
     env = gym.make("obstacle-v0")
-    env.reset()
+    obs = env.reset()
     done = False
-    while not done:
-        env.render()
-        action =  random.randint(0,4)# Your agent code here
+    xs_episode = []
+    ys_episode = []
+    xs = []
+    ys = []
+    rewards = []
+    rewards_episode = []
+    rewards = []
+    reward_sum = 0
+    episode_number = 0
+    running_reward = None
+    optimizer = torch.optim.RMSprop(model.parameters())
+    while True:
+        if render:
+            env.render()
+        with torch.no_grad:
+            action_probs =  model(obs)
+        prob_thresh = np.random.uniform()
+        action = np.argmax(action_probs)
+        if prob_thresh < 0.2:
+            action = env.action_space.sample() #random action for exploration
+        xs_episode.append(obs)
+        y = 2 if action == 2 else 0 #fake label
+        ys_episode.append(y)
         obs, reward, done, info = env.step(action)
-        
+        reward_sum += reward
+        rewards_episode.append(reward)
+        if done:
+            #episode finished
+            episode_number += 1
+            xs.append(xs_episode)
+            ys.append(ys)
+            xs_episode = []
+            ys_episode = []
+            discounted_rewards = model.discount_rewards(rewards_episode)
+            discounted_rewards = np.array(discounted_rewards)
+            discounted_rewards -= np.mean(discounted_rewards)
+            discounted_rewards /= np.std(discounted_rewards)
+            rewards.append(discounted_rewards)
+            rewards_episode = []
+
+            
+            if episode_number % batch_size == 0:
+                #reset training data
+                optimizer.zero_grad()
+                loss = None # To do
+                loss.backward()
+                for param in model.parameters():
+                    param.grad.data.clamp_(-1, 1)
+                optimizer.step()
+
+            obs = env.reset()
+            running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+            print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+            if episode_number % 100 == 0:
+                 torch.save(model.state_dict(), model_save_path)
+            reward_sum = 0
     env.close()
