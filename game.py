11a15c5c3f9214516a9e992bacf72a75ef619a33
lakshay taneja <lakshaytaneja26@gmail.com>
2019-04-11 17:14:49 -0500

    loss update

--- game.py
+++ game.py
@@ -2,60 +2,61 @@ import gym, obstacle_env, random
 from agent import model,batch_size, model_save_path
 import numpy as np
 import torch
-render = False
+render = True
 
 if __name__ == '__main__':
     env = gym.make("obstacle-v0")
     obs = env.reset()
     done = False
-    xs_episode = []
-    ys_episode = []
-    xs = []
+    rewards = []
     ys = []
-    rewards = []
-    rewards_episode = []
-    rewards = []
+    a = []
     reward_sum = 0
     episode_number = 0
     running_reward = None
+    loss = 0
     optimizer = torch.optim.RMSprop(model.parameters())
     while True:
         if render:
             env.render()
-        with torch.no_grad:
-            action_probs =  model(obs)
+        with torch.no_grad():
+            obs = torch.Tensor(obs)
+        action_probs = model(obs)
         prob_thresh = np.random.uniform()
-        action = np.argmax(action_probs)
-        if prob_thresh < 0.2:
-            action = env.action_space.sample() #random action for exploration
-        xs_episode.append(obs)
-        y = 2 if action == 2 else 0 #fake label
-        ys_episode.append(y)
+        action = torch.argmax(action_probs).item()
+        # if action == 0:
+        #     action = 4
+        # if prob_thresh < 0.3:
+        #     action = env.action_space.sample() + 1
+        #     if action > 4:
+        #         action = 1#random action for exploration
+        ys.append(action)
+        a.append(action_probs)
         obs, reward, done, info = env.step(action)
         reward_sum += reward
-        rewards_episode.append(reward)
+        rewards.append(reward)
         if done:
             #episode finished
             episode_number += 1
-            xs.append(xs_episode)
-            ys.append(ys)
-            xs_episode = []
-            ys_episode = []
-            discounted_rewards = model.discount_rewards(rewards_episode)
+
+            discounted_rewards = model.discount_rewards(torch.Tensor(rewards))
             discounted_rewards = np.array(discounted_rewards)
             discounted_rewards -= np.mean(discounted_rewards)
             discounted_rewards /= np.std(discounted_rewards)
-            rewards.append(discounted_rewards)
-            rewards_episode = []
-
+            loss += torch.sum(torch.nn.functional.cross_entropy(torch.stack(a), torch.LongTensor(ys)) * torch.Tensor(discounted_rewards))
+            ys = []
+            rewards = []
+            a = []
             
             if episode_number % batch_size == 0:
                 #reset training data
                 optimizer.zero_grad()
-                loss = None # To do
+                #states = torch.Tensor(xs)
                 loss.backward()
+                loss = 0
                 for param in model.parameters():
-                    param.grad.data.clamp_(-1, 1)
+
+                    param.grad.data = -1 * param.grad.data
                 optimizer.step()
 
             obs = env.reset()
